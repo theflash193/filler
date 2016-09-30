135d3954e09ea2d997458a85c8b515cf01d9d6d1
Gordon RASS-KWASI <grass-kw@e3r12p6.42.fr>
2016-09-30 17:19:06 +0200

    recuperation de la map

--- resources/src/main/main.c
+++ resources/src/main/main.c
@@ -6,11 +6,35 @@
 /*   By: grass-kw <grass-kw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/11 15:27:27 by grass-kw          #+#    #+#             */
-/*   Updated: 2016/09/30 16:31:16 by grass-kw         ###   ########.fr       */
+/*   Updated: 2016/09/30 17:18:19 by grass-kw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
+
+static void clear_env(t_env *e)
+{
+	if (e->map != NULL)
+		ft_free_tab(e->map);
+	ft_bzero(e, sizeof(t_env));
+}
+
+static void	ft_put_array_fd(char **tab, int fd)
+{
+	int	i;
+
+	i = 0;
+	ft_putendl_fd("array(", fd);
+	while (tab[i])
+	{
+		ft_putstr_fd("\t", fd);
+		ft_putnbr_fd(i, fd);
+		ft_putstr_fd(" => ", fd);
+		ft_putendl_fd(tab[i], fd);
+		i++;
+	}
+	ft_putendl_fd("    )", fd);
+}
 
 static void player_number(t_env *e)
 {
@@ -44,10 +68,20 @@ static void map(t_env *e)
 	char	*line;
 	int		i;
 
+	i = 0;
 	line = NULL;
 	map_dimension(e);
-	// e->map[i] = (char **)malloc(sizeof(char) * e->line + 1);
-	// get_next_line(0, &line);
+	e->map = (char **)malloc(sizeof(char *) * e->line + 1);
+	get_next_line(0, &line);
+	ft_strclean(line);
+	while (i < e->line)
+	{
+		get_next_line(0, &line);
+		e->map[i++] = ft_strdup(line + 4);
+		ft_strclean(line);
+	}
+	e->map[i] = 0;
+	ft_put_array_fd(e->map, 2);
 }
 
 int main(int ac, char **av)
@@ -56,6 +90,7 @@ int main(int ac, char **av)
 
 	ft_bzero(&e, sizeof(t_env));
 	player_number(&e);
-	map_dimension(&e);
+	map(&e);
+	clear_env(&e);
 	return (0);
 }
