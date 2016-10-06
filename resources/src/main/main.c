7c40ae93f0b47dda8a1455a3f10dd6ad88461bfc
Gordon RASS-KWASI <grass-kw@e1r2p16.42.fr>
2016-10-06 15:38:02 +0200

    debug en cours

--- resources/src/main/main.c
+++ resources/src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: grass-kw <grass-kw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/11 15:27:27 by grass-kw          #+#    #+#             */
-/*   Updated: 2016/10/06 12:07:36 by grass-kw         ###   ########.fr       */
+/*   Updated: 2016/10/06 14:41:59 by grass-kw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,26 +38,39 @@ static char	**try_place_piece(t_env *e, int x, int y, char **new_map)
 	if (x + e->piece_line > e->line || y + e->piece_colonne > e->colonne)
 		return (NULL);
 	i = x;
+	ft_putendl_fd("1", 2);
+	k = 0;
 	while (i < e->piece_line)
 	{
 		j = y;
-		k = l;
+		l = 0;
 		while (j < e->piece_colonne)
 		{
+			// ft_putendl_fd("2", 2);
 			if (is_ennemy(new_map[i][j], e) && e->piece[k][l] == NEW_PIECE)
 				return (NULL);
+			// ft_putendl_fd("3", 2);
 			if (form_replace_piece == 1 && (is_player(new_map[i][j], e) && e->piece[k][l] == NEW_PIECE))
 				return (NULL);
+			// ft_putendl_fd("4", 2);
 			if (form_replace_piece == 0 && is_player(new_map[i][j], e))
 				form_replace_piece = 1;
+			// ft_putendl_fd("5", 2);
 			if (new_map[i][j] == EMPTY && e->piece[k][l] == NEW_PIECE)
 				new_map[i][j] = e->player_lm;
+			// ft_putendl_fd("6", 2);
 			j++;
 			l++;
 		}
 		i++;
 		k++;
 	}
+	if (form_replace_piece != 1)
+	{
+		ft_putendl_fd("pas d'emplacement", 2);
+		return (NULL);
+	}
+	ft_putendl_fd("7", 2);
 	return (new_map);
 }
 
@@ -92,23 +105,21 @@ static void find_all_possibility(t_env *e)
 		while (j < e->colonne)
 		{
 			new_map = array_cpy(e->map);
-			ft_put_array_fd(new_map, 2);
 			if (try_place_piece(e, i, j, e->map) != NULL)
 			{
+				ft_put_array_fd(new_map, 2);
 				e->choice_x = i;
 				e->choice_y = j;
 				return ;
 			}
 			ft_free_tab(new_map);
-			// ft_putendl_fd("1", 2);
 			j++;
 		}
 		i++;
 	}
-	// ft_putendl_fd("2", 2);
 	e->choice_x = 0;
 	e->choice_y = 0;
-	e->game_over = 1;
+	e->game_continue = 0;
 }
 
 static void	final_decision(t_env *e)
@@ -130,20 +141,19 @@ int main(int ac, char **av)
 
 	ft_bzero(&e, sizeof(t_env));
 	player_number(&e);
-	// while (e.game_over == 0)
-	// {
-		// ft_putendl_fd("nouveau tour", 2);
-		// ft_putendl_fd("map", 2);
+	while (e.game_continue)
+	{
+		ft_putendl_fd("nouveau tour", 2);
+		ft_putendl_fd("map", 2);
 		map(&e);
-		// ft_putendl_fd("piece", 2);
+		ft_putendl_fd("piece", 2);
 		piece(&e);
-		// ft_putendl_fd("thinking_startegy", 2);
-		// thinking_strategy(&e);
-		// ft_putendl_fd("final_decision", 2);
+		ft_putendl_fd("thinking_startegy", 2);
+		thinking_strategy(&e);
+		ft_putendl_fd("final_decision", 2);
 		final_decision(&e);
-		// prepare_next_turn(&e);
-
-	// }
+		prepare_next_turn(&e);
+	}
 	// ft_putendl_fd("3", 2);
 	clear_env(&e);
 	return (0);
