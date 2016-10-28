690b1a546081c90550a1cbb3b91ec46ca5370c78
theflash193 <flash.gordon77176@gmail.com>
2016-10-28 23:14:25 +0200

    suppression de la liste de possibilite a chaque tour

--- resources/src/main/main.c
+++ resources/src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: grass-kw <grass-kw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/11 15:27:27 by grass-kw          #+#    #+#             */
-/*   Updated: 2016/10/28 22:57:31 by ozdek            ###   ########.fr       */
+/*   Updated: 2016/10/28 23:13:42 by ozdek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -186,6 +186,7 @@ static void	final_decision(t_env *e)
 
 void	selectionne_la_meilleur_possibilite(t_env *e)
 {
+	ft_lstiter(e->liste_possibilite, print_possibilite);
 }
 
 static void	thinking_strategy(t_env *e)
@@ -205,6 +206,11 @@ static void	preparation_du_prochain_tour(t_env *e)
 	{
 		ft_free_tab(e->piece);
 		e->piece = NULL;
+	}
+	if (e->liste_possibilite != 0)
+	{
+		ft_lstdel(&(e->liste_possibilite), delete_map);
+		e->liste_possibilite = NULL;
 	}
 	e->choice_x = 0;
 	e->choice_y = 0;
