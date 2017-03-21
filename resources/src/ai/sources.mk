728c687ad092709ae094124b0f88583e32618d9f
Gordon RASS-KWASI <grass-kw@e1r2p23.42.fr>
2017-03-21 19:20:27 +0100

    creation fichier influence.c

--- resources/src/ai/sources.mk
+++ resources/src/ai/sources.mk
@@ -1,6 +1,6 @@
 
 MAIN	:= find_all_possibility.c thinking_strategy.c piece_non_inserable.c \
-	insertion_de_piece.c final_decision.c
+	insertion_de_piece.c final_decision.c influence.c
 
 OBJ		:= $(OBJ) $(MAIN:.c=.o)
 
--- resources/src/main/influence.c
+++ resources/src/main/influence.c
@@ -12,29 +12,6 @@
 
 #include "filler.h"
 
-void	influence(t_list *elem)
-{
-	t_map *choix;
-
-	choix = (t_map *)elem->content;
-	if (choix->player == 1)
-	{
-		choix->influence_p1 = array_cpy(choix->map);
-		marquage_influence(choix, 1);
-		choix->influence_p2 = array_cpy(choix->influence_p1);
-		marquage_influence(choix, 2);
-	}
-	else
-	{
-		choix->influence_p2 = array_cpy(choix->map);
-		marquage_influence(choix, 2);
-		choix->influence_p1 = array_cpy(choix->influence_p2);
-		marquage_influence(choix, 1);
-	}
-	choix->points_influence_p1 = calcul_influence(choix->influence_p1, 1);
-	choix->points_influence_p2 = calcul_influence(choix->influence_p2, 2);
-}
-
 int		calcul_influence(char **map, int player)
 {
 	int count;
