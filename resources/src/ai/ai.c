3b21e860181cab91bce719250bcd3ca241bf5e63
Gordon RASS-KWASI <grass-kw@e2r2p8.42.fr>
2017-07-18 21:15:16 +0200

    impression de la piece sur le nouveau plateau est reussi

--- resources/src/ai/ai.c
+++ resources/src/ai/ai.c
@@ -14,8 +14,8 @@
 
 int placement_possible(int x, int y, t_env *e)
 {
-	t_coord	p;
-	t_coord	i;
+	t_coord		p;
+	t_coord		i;
 	int			recouvre_forme;
 
 	recouvre_forme = 0;
@@ -45,6 +45,55 @@ int placement_possible(int x, int y, t_env *e)
 	return (1);
 }
 
+void	impression_plateau(t_env *e, int i, int j, t_entite	a)
+{
+	t_coord piece;
+	t_coord	plateau;
+
+	piece.x = 0;
+	plateau.x = i;
+	while (piece.x < e->piece.x)
+	{
+		piece.y = 0;
+		plateau.y = j;
+		while (piece.y < e->piece.y)
+		{
+			if (e->piece.entite[piece.x][piece.y] == '*' && a.entite[plateau.x][plateau.y] != 'o')
+			{
+				if (e->numero_joueur == 1)
+					a.entite[plateau.x][plateau.y] = 'o';
+				else
+					a.entite[plateau.x][plateau.y] = 'x';
+			}
+			piece.y++;
+			plateau.y++;
+		}
+		plateau.x++;
+		piece.x++;
+	}
+	core_entite(a);
+}
+
+void	initialisation_entite(t_env *e, int i, int j)
+{
+	char	**plateau;
+	int		count;
+
+	count = 0;
+	plateau = (char **)malloc(sizeof(char *) * e->plateau.x + 1);
+	while (count < e->plateau.x)
+	{
+		plateau[count] = ft_strdup(e->plateau.entite[count]);
+		count++;
+	}
+	plateau[count] = 0;
+	t_entite a;
+	a.entite = plateau;
+	a.x = e->plateau.x;
+	a.y = e->plateau.y;
+	impression_plateau(e, i, j, a);
+}
+
 void	ai(t_env *e)
 {
 	int i;
@@ -58,8 +107,10 @@ void	ai(t_env *e)
 		{
 			if (placement_possible(i, j, e) == 1)
 			{
+				initialisation_entite(e, i, j);
 				e->reponse.x = i;
 				e->reponse.y = j;
+				e->loop = 0;
 				return ;
 			}
 			j++;
